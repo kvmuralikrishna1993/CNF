419397621689a87cc6f430fe8b71ec9ef1e94835
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-12-24 15:38:37 +0530

    exam2

--- exam2/client.py
+++ exam2/client.py
@@ -1,23 +1,31 @@
 import socket
 import os,signal
 
+'''check the connections using numbers
+        on client and server side of program.'''
+
 def main():
     host = '127.0.0.1'
-    port = 5003
+    port = 5005
     s = socket.socket()
     s.connect((host,port))
     print("MARK-ATTENDANCE CHECK")
     print("------------------------------------")
     message = input("Enter your ROll NO: ")
     while message != "q":
-        s.send(message.encode())
-        data = s.recv(1024).decode()
-        message = input(str(data) + ": ")
-        data = s.recv(1024).decode()
-        print(str(data))
-        if str(data) == "ATTENDANCE SUCCESS":
+        s.send(str(message).encode()) #1
+        # if recieved is --> 3! <-- then connection will break
+        data = s.recv(1024).decode() #4
+        if str(data) == "ROLLNUMBER-NOTFOUND":
+            print(data)
             break
-        message = input("Enter your ROll NO: ")
+        message = input(str(data) + ": ") 
+        # sending answers for questions
+        s.send(str(message).encode()) #5
+        #recieving verification
+        data = s.recv(1024).decode() #8
+        print(str(data))
+        break
     s.close()
 if __name__ == '__main__':
     main()
--- exam2/server.py
+++ exam2/server.py
@@ -3,39 +3,46 @@ import os
 import threading
 import csv
 
+'''check the connections using numbers
+        on client and server side of program.'''
+
 def main():
     host = '127.0.0.1'
-    port = 5003
+    port = 5005
     s = socket.socket()
     s.bind((host,port))
     print('server started: ')
     s.listen(5)
     info = []
+    #path assigned depending on my folder path
+    #reading the csv file
     f = open('/Users/Muralikrishna/Desktop/CNF/exam2/data.csv', 'r')
     csvreader = csv.reader(f, delimiter=",")
     for row in csvreader:
         info.append(row)
-
+    f.close()
+    #connecting client and server
+    #creating new thread for client
     while True:
         c, addr = s.accept()
-        roll = c.recv(1024).decode()
+        roll = c.recv(1024).decode() #2
         print('Connected User: ' + roll)
-        threading.Thread(target = question, args = (str(roll), c, info)).start()
-        #start_new_thread( question, (str(roll), c, info))
+        threading.Thread(target = attendance, args = (str(roll), c, info)).start()
     s.close()
 
-def question(roll,c,info):
+#attendance function used by thread.
+def attendance(roll,c,info):
+    flag = 1
     for row in info:
+        flag = flag + 1
         if roll in row:
-            c.send(str(row[1]).encode())
-            if str(c.recv(1024).decode()) == str(row[2]):
-                c.send("ATTENDANCE SUCCESS".encode())
+            c.send(str(row[1]).encode()) #3
+            if str(c.recv(1024).decode()) == str(row[2]): #6
+                c.send("ATTENDANCE SUCCESS".encode()) #7
             else:
-                c.send("ATTENDANCE FAILUE".decode())
-        else:
-            c.send("ROLLNUMBER-NOTFOUND".encode())
-            break
+                c.send("ATTENDANCE FAILUE".decode()) #7
+    if (flag > len(info)):
+        c.send("ROLLNUMBER-NOTFOUND".encode()) #--> 3! <----
                 
 if __name__ == '__main__':
     main()
- 
