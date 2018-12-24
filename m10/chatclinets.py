8a49db7a9dfdc27a226358bb6f8eceabfdeda906
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-12-24 15:42:26 +0530

    m10

---
+++ m10/chatclinets.py
@@ -0,0 +1,34 @@
+import socket
+from threading import *
+import os,signal
+#main
+def Main():
+    host = '10.1.133.72'
+    port = 1304
+    s = socket.socket()
+    try:
+        s.connect((host, port))
+    except OSError as e:
+        print("Sorry, you are Late..")
+        return
+    #thread for client
+    thread2 = Thread(target = send, args = (s,)).start()
+    #disconnecting from chatroom
+    while True:
+        data = s.recv(1024).decode()
+        if (data == 'Disconnect'):
+            os.kill(os.getpid(), signal.CTRL_BREAK_EVENT)
+            break
+        print(data)
+    s.close()
+#function to send to clients..
+def send(s):
+    while True:
+        msg = input()
+        # print('MSG Sening')
+        s.send(msg.encode())
+    s.close()
+    
+
+if __name__ == '__main__':
+    Main()
---
+++ m10/chatroomserver.py
@@ -0,0 +1,82 @@
+import socket
+import os
+from threading import *
+import time
+import signal
+#main function
+def Main():
+	host = '10.1.133.72'
+	port = 1973
+	s = socket.socket()
+	s.bind((host,port))
+	print("Server Started....")
+	#can chat up to 10 users
+	s.listen(10)
+	#connections list
+	clientlist = []
+	#dictionary of id names and connections
+	#where, key is connection; value is id
+	connectiondictionary = {}
+	#connecting with each client.
+	while True:
+		c, addr = s.accept()
+		c.send("=========W3LC0M3 T0 CH4TR00M=========".encode())
+		c.send('Enter your Name: '.encode())
+		clientid = c.recv(1024)
+		print('Connected User: ' + clientid.decode())
+		connectiondictionary[c] = str(clientid.decode())
+		clientlist.append(c)
+		for connection in clientlist:
+			if c != connection:
+				connection.send((connectiondictionary[c] + ' is Connected.').encode())
+		#setting thread for each client.
+		Thread(target = chatclients, args = (c, addr, clientlist, connectiondictionary)).start()
+	#closing server
+	s.close()
+#-----------------------------------------------------------------
+#checkclientsing number of connections
+#if only one shuts after 10 sec..
+def checkclients():
+	print(active_count())
+	if (active_count() == 2):
+		print('Waiting for Connections....')
+		time.sleep(10)
+		if (active_count() == 2):
+			os.kill(os.getpid(), signal.CTRL_BREAK_EVENT)
+#-----------------------------------------------------------------
+#chatroom function thread
+def chatclients(c, addr, clientlist, connectiondictionary):
+	while True:
+		try:
+			message = (c.recv(1024)).decode()
+			print(connectiondictionary[c] + '<-->' + message)
+			if message != 'x' and c in clientlist:
+				for client in clientlist:
+					if c != client:
+						try:
+							clientname = connectiondictionary[c]
+							client.send((clientname + '-->' + message).encode())
+						except:
+							c.close()
+							removeclient(c, clientlist)
+			else:
+				c.send(('Do you want to Exit?(Y/N)').encode())
+				if ((c.recv(1024)).decode() == 'Y'):
+					for con in clientlist:
+						if c != con:
+							con.send((connectiondictionary[c] + ' is disconnected. Users online: ' + str(active_count() - 2)).encode())
+					c.send('Disconnect'.encode())
+					removeclient(c,clientlist)
+					checkclients()
+					return 1
+		except:
+			continue
+	c.close()
+#------------------------------------------------------------------------------
+#removing client from chatroom
+def removeclient(c, clientlist):
+	if c in clientlist:
+		clientlist.removeclient(c)
+#-----------------------------------------------------------------------------
+if __name__ == '__main__':
+	Main()
